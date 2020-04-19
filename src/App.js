e8f27be01e5722f63e22f671830121e3a96833bd
AlQaholic007 <soham.parekh1998@gmail.com>
2020-04-19 21:43:03 +0530

    Fully functional coronavirus tracker

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { Cards, Chart, CountryPicker } from './Components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: ''
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country })
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData })
+  }
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} alt="COVID-19" src="https://i.ibb.co/7QpKsCX/image.png"/>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange}/>
+        <Chart data={data} country={country}/>
+      </div>
+    )
+  }
+}
+
+export default App;
